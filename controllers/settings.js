5d55a42de79585fea836a65bdfc7a697c0ab4f01
agatdwi <agatdwisubaktiyan444@gmail.com>
2024-01-13 19:00:34 +0700

    uplload

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "AgatZ";
+global.key_free = ["gattfree"];
+global.key_premium = ['agatz'];
+global.limit_free = "9999";
+global.limit_premium = "99999";
+global.dateLimit = "Oktober, 31 2030",
+global.mongo_Db = "mongodb+srv://Agat:agatdwi11@cluster0.vbpssel.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "agatdwisubakti335@gmail.com";
+global.my_email_password = "iyzfvwriupbxzlrq";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
