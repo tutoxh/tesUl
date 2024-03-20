cf9e8f830aaac91e13284161824e115bd6d73ebf
agatzdev <74965339+agatdwi@users.noreply.github.com>
2024-03-20 10:07:54 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -7,7 +7,7 @@ global.dateLimit = "Oktober, 31 2022",
 global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
-global.my_email = "agatdwisubakti335@gmail.com";
+global.my_email = "chaikaloffc@gmail.com";
 global.my_email_password = "iyzfvwriupbxzlrq";
 global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
 global.port = 8080
