1f91efd2ecb60e082ce04d920a4d5aa6f731ffc7
agatzdev <74965339+agatdwi@users.noreply.github.com>
2024-03-20 10:04:15 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,9 +1,9 @@
 global.author = "AgatZ";
 global.key_free = ["gattfree"];
 global.key_premium = ['agatz'];
-global.limit_free = "1500";
-global.limit_premium = "2000";
-global.dateLimit = "Oktober, 31 2040",
+global.limit_free = "9999999999";
+global.limit_premium = "9999999999";
+global.dateLimit = "Oktober, 31 2022",
 global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
