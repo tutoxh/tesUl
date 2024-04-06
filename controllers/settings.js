487124633fae364f3c57ffec767c46df2bedb3a2
tutoxh <103659233+tutoxh@users.noreply.github.com>
2024-04-06 16:08:20 -0300

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -4,10 +4,10 @@ global.key_premium = ['agatz'];
 global.limit_free = "9999999999";
 global.limit_premium = "9999999999";
 global.dateLimit = "Oktober, 31 2022",
-global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.mongo_Db = "mongodb+srv://db-dylux:sennapass@cluster0.szhyr3s.mongodb.net/?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
-global.my_email = "chaikaloffc@gmail.com";
-global.my_email_password = "iyzfvwriupbxzlrq";
+global.my_email = "fg98ff@gmail.com";
+global.my_email_password = "zqdzayhkwnngqbtt";
 global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
 global.port = 8080
