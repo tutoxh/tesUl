841727ef6d6052ea4445032a60e925f0bdf2229c
tutoxh <103659233+tutoxh@users.noreply.github.com>
2024-04-06 17:02:16 -0300

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -4,7 +4,7 @@ global.key_premium = ['agatz'];
 global.limit_free = "9999999999";
 global.limit_premium = "9999999999";
 global.dateLimit = "Oktober, 31 2022",
-global.mongo_Db = "mongodb+srv://db-dylux:sennapass@cluster0.szhyr3s.mongodb.net/?retryWrites=true&w=majority";
+global.mongo_Db = "mongodb+srv://dbdyluxbot:senna1234@cluster0.xwbxda5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
 global.my_email = "fg98ff@gmail.com";
